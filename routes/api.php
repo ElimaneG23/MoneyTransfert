d21353024fef9360c5757f11eea4388b05c32b2a
ElimaneG23 <gelimane23@gmail.com>
2026-04-09 13:41:41 +0000

    creation des utilasateurs

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController; // <- Important
 
+// Route pour lister tous les users (Read)
+Route::get('/users', [UserController::class, 'index']);
+
+// Route pour créer un nouvel user (Create)
+Route::post('/user', [UserController::class, 'store']);
+
+// Route par défaut Laravel pour l'utilisateur connecté (auth:sanctum)
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
